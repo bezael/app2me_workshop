c47fed53b83be5afc1e1e6cde7cf41184e5dc725
bezael <bezael@gmail.com>
2024-10-10 18:32:02 +0200

    remove unused

--- projects/app2me/src/app/app.component.ts
+++ projects/app2me/src/app/app.component.ts
@@ -1,28 +1,15 @@
-import { Component, inject } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { DominicodeUIComponent } from '@dominicode-ui';
 import { ButtonUIComponent } from '@dominicode-ui/button';
-import { DominicodeUIService } from '@dominicode-ui/index';
-// import { DominicodeUIService } from '@dominicode-ui/DominicodeUIService';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet, ButtonUIComponent, DominicodeUIComponent],
-  // providers: [DominicodeUIService],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'app2me';
-  private readonly _dominicodeUIService = inject(DominicodeUIService);
-
-  login() {
-    const user = { id: '1', name: 'Juan', email: 'juan@example.com' };
-    this._dominicodeUIService.setLoggedUser(user);
-  }
-
-  logout() {
-    this._dominicodeUIService.clearLoggedUser();
-  }
 }
