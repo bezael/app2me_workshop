28f0584813ee3a5682f2be810ee920bf38f9e231
bezael <bezael@gmail.com>
2024-10-11 07:32:03 +0200

    add solution

--- projects/dominicode-ui/src/lib/dominicode-ui.service.spec.ts
+++ projects/dominicode-ui/src/lib/dominicode-ui.service.spec.ts
@@ -1,5 +1,5 @@
 import { TestBed } from '@angular/core/testing';
-import { DominicodeUIService } from './dominicode-ui.service';
+import { DominicodeUIService, Product } from './dominicode-ui.service';
 
 describe('DominicodeUiService', () => {
   let service: DominicodeUIService;
@@ -11,5 +11,18 @@ describe('DominicodeUiService', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should return an observable of products', (done) => {
+    const expectedProducts: Product[] = [
+      { id: '1', name: 'Product 1', price: 100 },
+      { id: '2', name: 'Product 2', price: 200 },
+      { id: '3', name: 'Product 3', price: 300 },
+    ];
+
+    service.getAllProducts().subscribe((products) => {
+      expect(products).toEqual(expectedProducts);
+      done();
+    });
   });
 });
