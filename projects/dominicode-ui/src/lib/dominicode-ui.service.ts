056cb61afc3b3736d7a0badd11394c43a908cc48
bezael <bezael@gmail.com>
2024-10-11 06:59:56 +0200

    add method to service

--- projects/dominicode-ui/src/lib/dominicode-ui.service.ts
+++ projects/dominicode-ui/src/lib/dominicode-ui.service.ts
@@ -1,4 +1,5 @@
-import { Injectable, signal } from '@angular/core';
+import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
 
 export interface User {
   id: string;
@@ -6,9 +7,22 @@ export interface User {
   email: string;
 }
 
+export interface Product {
+  id: string;
+  name: string;
+  price: number;
+}
+
 @Injectable({
   providedIn: 'root',
 })
 export class DominicodeUIService {
-
+  getAllProducts(): Observable<Product[]> {
+    const products: Product[] = [
+      { id: '1', name: 'Product 1', price: 100 },
+      { id: '2', name: 'Product 2', price: 200 },
+      { id: '3', name: 'Product 3', price: 300 },
+    ];
+    return of(products);
+  }
 }
